84091f631236128d24bde149aec55c8d3921a2ac
unknown <you@example.com>
2020-09-13 17:38:25 +0100

    added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4001;
+const port = process.env.PORT || 4001;
 const parser = require('body-parser');
 const logger = require('morgan');
 const cors = require('cors');
